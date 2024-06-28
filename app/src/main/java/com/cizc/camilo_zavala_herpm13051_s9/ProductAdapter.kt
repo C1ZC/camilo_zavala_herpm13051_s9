1cef669d9bbd69d93d4c3b5d77f717a71555463c
C1ZC <camilo.zavala.c@gmail.com>
2024-06-27 21:16:06 -0400

    crear

--- app/src/main/java/com/cizc/camilo_zavala_herpm13051_s9/ProductAdapter.kt
+++ app/src/main/java/com/cizc/camilo_zavala_herpm13051_s9/ProductAdapter.kt
@@ -7,7 +7,6 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
-
 class ProductAdapter(private var products: MutableList<Product>) : RecyclerView.Adapter<ProductAdapter.ProductViewHolder>() {
 
     class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
